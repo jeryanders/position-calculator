20d2ef0cea603e320828eba99c2bf2e60fa09053
jeryanders <jeryanders@gmail.com>
2016-03-31 14:40:22 -0400

    Type definitions for Position Calculator
    Project: https://github.com/tlindig/position-calculator

---
+++ position-calculator.d.ts
@@ -0,0 +1,34 @@
+﻿// Type definitions for Position Calculator
+// Project: https://github.com/tlindig/position-calculator
+// Definition by: Jesse Anderson <https://github.com/jeryanders>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+/// <reference path="../jquery/jquery.d.ts"/>
+
+interface CalculatorConfig {
+    target: JQueryCoordinates;
+    targetAt: string;
+    item: JQuery,
+    itemAt: string;
+    flip: string;
+}
+
+interface ActivePosition {
+    y: number;
+    x: number;
+}
+
+interface Calculated {
+    moveBy: ActivePosition;
+}
+
+interface Calculator {
+    calculate(): Calculated;
+}
+
+interface JQueryStatic {
+    PositionCalculator(options: CalculatorConfig): Calculator;
+}
+
+declare module 'position-calculator' {
+    
+}
